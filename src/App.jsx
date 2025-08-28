d3eb6eeb55852ac162cec75a2a85792e185ed7d2
testloto <chitranisha08@gmail.com>
2025-08-28 11:55:49 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-4xl text-amber-200'>
+      <h1 className='text-6xl'>chitra sarkar</h1>
+      
+    </div>
+  )
+}
+
+export default App
