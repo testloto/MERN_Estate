25d4d49c61ea5db66a73670b6ac18de26dcfaccb
testloto <chitranisha08@gmail.com>
2025-08-31 10:29:58 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
-
-
+import connectDB from './database/db.js'
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
 
+
+connectDB();
 
 app.listen(3000,()=>{
     console.log('Server is running on the port 3000')
