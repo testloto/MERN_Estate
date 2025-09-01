b4b6e25db408b2d038b8feec9cfe710f02fc234b
testloto <chitranisha08@gmail.com>
2025-09-01 09:16:35 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,14 +2,22 @@ import express from 'express';
 import connectDB from './database/db.js'
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
+
+
+
 const app = express();
 dotenv.config();
 
 
 connectDB();
+app.use(express.json()); // send the req.body 
+
 
 app.listen(3000,()=>{
     console.log('Server is running on the port 3000')
 })
 
-app.use('/api/user',userRouter)
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,21 +4,22 @@ const userSchema = new mongoose.Schema({
     username:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        trim:true
     },
     email:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        trim:true
     },
     password:{
         type:String,
-        required:true,
-        
+        required:true,  
     }
 
 },
 {timestamps:true}
 )
 
-export const User = mongoose.Schema("User",userSchema)
+export const User = mongoose.model("User",userSchema)
