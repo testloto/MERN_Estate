ca45c309ef18eae71c060e75953b7d4bef92fcc9
testloto <chitranisha08@gmail.com>
2025-09-02 16:59:18 +0530

    sign in api routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,47 @@
-import {User} from "../models/user.model.js";
-import bcrypt from 'bcrypt'
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
   // console.log(req.body)
 
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password,10);
-  const newUser = new User({ username, email, password:hashedPassword });
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
 
   try {
-    
-      await newUser.save();
-    
-      res.status(201).json("user created successfully")
+    await newUser.save();
+
+    res.status(201).json("user created successfully");
   } catch (error) {
-    // res.status(500).json(error.message) // simple way of getting an error not in detail 
-    next(error); // comprehensive way to get error 
-    
+    // res.status(500).json(error.message) // simple way of getting an error not in detail
+    next(error); // comprehensive way to get error
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // Find user but exclude the password
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credential"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // Fetch user again without password before sending response
+    const userWithoutPassword = await User.findById(validUser._id).select("-password");
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(userWithoutPassword);
+  } catch (error) {
+    next(error);
   }
 };
